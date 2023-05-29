e512a9926c99cb6eb6a93afe36725996aa34a312
Alvin Ashcraft <alashcraft@gmail.com>
2023-05-29 11:43:34 -0400

    Add starter project for chapter 4

---
+++ Chapter04/Start/MyMediaCollection/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+using Microsoft.UI.Xaml;
+using MyMediaCollection.ViewModels;
+
+namespace MyMediaCollection
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        public MainViewModel ViewModel => App.ViewModel;
+    }
+}
