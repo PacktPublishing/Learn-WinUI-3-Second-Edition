21e22a0369885d42508937343ef11d4736cd18e0
Alvin Ashcraft <alashcraft@gmail.com>
2023-07-23 12:06:52 -0400

    Add starter code for chapter 8
    No modifications for Complete folder yet.

---
+++ Chapter08/Complete/MyMediaCollection/Interfaces/INavigationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMediaCollection.Interfaces
+{
+    public interface INavigationService
+    {
+        string CurrentPage { get; }
+        void NavigateTo(string page);
+        void NavigateTo(string page, object parameter);
+        void GoBack();
+    }
+}
---
+++ Chapter08/Start/MyMediaCollection/Interfaces/IValidatable.cs
@@ -0,0 +1,7 @@
+﻿namespace MyMediaCollection.Interfaces
+{
+    public interface IValidatable
+    {
+        void Validate(string memberName, object value);
+    }
+}
