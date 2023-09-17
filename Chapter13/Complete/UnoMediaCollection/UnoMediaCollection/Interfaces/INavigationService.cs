800f0c3a8a20f95a5b4c78741c5138685bf79d0b
Alvin Ashcraft <alashcraft@gmail.com>
2023-09-17 13:18:00 -0400

    Remove namespaces

--- Chapter13/Complete/UnoMediaCollection/UnoMediaCollection/Interfaces/INavigationService.cs
+++ Chapter13/Complete/UnoMediaCollection/UnoMediaCollection/Interfaces/INavigationService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace UnoMediaCollection.Interfaces
+﻿namespace UnoMediaCollection.Interfaces
 {
     public interface INavigationService
     {
--- Chapter13/Complete/UnoMediaCollection/UnoMediaCollection/ItemDetailsPage.xaml.cs
+++ Chapter13/Complete/UnoMediaCollection/UnoMediaCollection/ItemDetailsPage.xaml.cs
@@ -1,16 +1,4 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using UnoMediaCollection.ViewModels;
+﻿using UnoMediaCollection.ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
